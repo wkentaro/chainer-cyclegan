213775e0d9fa99af75bb552ad2ff7159662a4e25
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-12-24 04:50:07 +0900

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
+import glob
+import os.path as osp
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+
 
 df = pd.read_csv('logs/log.csv')
 
@@ -30,5 +34,10 @@ plt.legend()
 ax = plt.subplot(235)
 df.plot(x='iteration', y=['loss_D_A', 'loss_D_B'], ax=ax)
 plt.legend()
+
+img_file = sorted(glob.glob('logs/*.jpg'), reverse=True)[0]
+plt.subplot(236)
+plt.imshow(plt.imread(img_file))
+plt.title(osp.basename(img_file))
 
 plt.show()
