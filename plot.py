990453cc2c826f8dee4f8dfe5abe7c83ac136db1
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-12-24 15:35:19 +0900

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 
+import argparse
 import glob
 import os.path as osp
 
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
 
 
-df = pd.read_csv('logs/log.csv')
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('log_dir')
+args = parser.parse_args()
+
+
+df = pd.read_csv(osp.join(args.log_dir, 'log.csv'))
 
 plt.figure(dpi=100, figsize=(16, 8))
 
@@ -32,7 +38,7 @@ ax = plt.subplot(235)
 df.plot(x='iteration', y=['loss_D_A', 'loss_D_B'], ax=ax)
 plt.legend()
 
-img_files = glob.glob('logs/*.jpg')
+img_files = glob.glob(osp.join(args.log_dir, '*.jpg'))
 if img_files:
     img_file = sorted(img_files, reverse=True)[0]
     plt.subplot(236)
