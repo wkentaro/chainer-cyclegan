8103dec64a203b57f3626d4dbd983eff4c9bb7d0
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-12-24 18:31:40 +0900

    Fix for flake8: generate_D_random_pth.py

--- generate_D_random_pth.py
+++ generate_D_random_pth.py
@@ -3,10 +3,7 @@
 import os.path as osp
 import sys
 
-import numpy as np
 import torch
-from torch.autograd import Variable
-
 
 here = osp.dirname(osp.realpath(__file__))
 pytorch_dir = osp.join(here, 'src/pytorch-cyclegan')
@@ -15,14 +12,18 @@ sys.path.insert(0, pytorch_dir)
 from models import networks
 
 
-norm_layer = networks.get_norm_layer(norm_type='instance')
-model = networks.NLayerDiscriminator(
-    input_nc=3,
-    ndf=64,
-    norm_layer=norm_layer,
-    use_sigmoid=False,
-    gpu_ids=[0],
-)
-out_file = osp.join(here, 'data/D_random.pth')
-torch.save(model.state_dict(), out_file)
-print('==> {:s}'.format(out_file))
+def main():
+    model = networks.NLayerDiscriminator(
+        input_nc=3,
+        ndf=64,
+        norm_layer=networks.get_norm_layer(norm_type='instance'),
+        use_sigmoid=False,
+        gpu_ids=[0],
+    )
+    out_file = osp.join(here, 'data/D_random.pth')
+    torch.save(model.state_dict(), out_file)
+    print('Saved model file: {:s}'.format(out_file))
+
+
+if __name__ == '__main__':
+    main()
