c87c11cf321447e9737c0ea6eb0e6ac3ef987e5a
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-12-26 19:48:46 +0900

    Stop using RandomIndexing

--- examples/male2female/train.py
+++ examples/male2female/train.py
@@ -19,19 +19,19 @@ sys.path.insert(0, osp.join(here, '../horse2zebra'))
 from train import train
 
 
-class RandomIndexingDataset(object):
-
-    def __init__(self, dataset, size=1000):
-        self._dataset = dataset
-        self._dataset_size = len(self._dataset)
-        self._size = size
-
-    def __len__(self):
-        return self._size
-
-    def __getitem__(self, index):
-        index2 = np.random.randint(0, self._dataset_size)
-        return self._dataset[index2]
+# class RandomIndexingDataset(object):
+#
+#     def __init__(self, dataset, size=1000):
+#         self._dataset = dataset
+#         self._dataset_size = len(self._dataset)
+#         self._size = size
+#
+#     def __len__(self):
+#         return self._size
+#
+#     def __getitem__(self, index):
+#         index2 = np.random.randint(0, self._dataset_size)
+#         return self._dataset[index2]
 
 
 if __name__ == '__main__':
@@ -44,11 +44,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     dataset_train = chainer.datasets.TransformDataset(
-        RandomIndexingDataset(Male2FemaleDataset('train')),
+        Male2FemaleDataset('train'),
         CycleGANTransform(load_size=(314, 256), fine_size=(256, 256)),
     )
     dataset_test = chainer.datasets.TransformDataset(
-        RandomIndexingDataset(Male2FemaleDataset('test')),
+        Male2FemaleDataset('test'),
         CycleGANTransform(load_size=(314, 256), fine_size=(256, 256)),
     )
 
