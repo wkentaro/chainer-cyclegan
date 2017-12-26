90a174c58e1da3bee818b4f2e581b12a1414d3c3
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-12-26 21:56:39 +0900

    Download image from URL

--- examples/horse2zebra/download_models.sh
+++ examples/horse2zebra/download_models.sh
@@ -2,6 +2,11 @@
 
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+set -x
+set -e
+
+# ----------------------------------------------------------------------------------------------------------
+
 URL=https://people.eecs.berkeley.edu/~taesung_park/pytorch-CycleGAN-and-pix2pix/models/horse2zebra.pth
 OUTPUT=$HERE/data/G_horse2zebra.pth
 
@@ -9,4 +14,16 @@ if [ -e $OUTPUT ]; then
   echo "File already exists: $OUTPUT"
 else
   wget $URL -O $OUTPUT
+fi
+
+# ----------------------------------------------------------------------------------------------------------
+
+URL=https://drive.google.com/uc?id=1j3D4UrKUN5RZW5qDQgy7tJnWtnQeV-Tc
+OUTPUT=$HERE/data/G_horse2zebra_from_pytorch.npz
+
+if [ -e $OUTPUT ]; then
+  echo "File already exists: $OUTPUT"
+else
+  which gdown &>/dev/null || pip install gdown
+  gdown $URL -O $OUTPUT
 fi
